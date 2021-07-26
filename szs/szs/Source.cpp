30c92551d6bf175537381966ac717c8e28a70aa3
Nikita <Nikpro200125@gmail.com>
2021-07-26 23:04:27 +0700

    Update Source.cpp

--- szs/szs/Source.cpp
+++ szs/szs/Source.cpp
@@ -6,19 +6,20 @@
 #include <future>
 #include <algorithm>
 #include <boost/container/flat_set.hpp>
+#include <boost/container/flat_map.hpp>
 #include <boost/algorithm/string.hpp>
 #include "Structure.cpp"
 
 using namespace std;
 
-bool search(vector<string>&, string&, boost::container::flat_set<string>&);
-void r(const vector<vector<char>>&, string&, const int&, const int&, Structure&, vector<vector<bool>>&, boost::container::flat_set<string>&);
+bool search(vector<string>&, string&, boost::container::flat_set<string>&, vector<pair<int, int>>&, boost::container::flat_map<string, vector<pair<int, int>>>&);
+void r(const vector<vector<char>>&, string&, const int&, const int&, Structure&, vector<vector<bool>>&, boost::container::flat_set<string>&, vector<pair<int, int>>&, boost::container::flat_map<string, vector<pair<int, int>>>&);
 void getNeighbors(const vector<vector<char>>&, const int&, const int&, vector<pair<int, int>>&, vector<vector<bool>>&);
 
 int main() {
 		SetConsoleOutputCP(1251);
 		SetConsoleCP(1251);
-		ifstream in("dic2.txt");
+		ifstream in("dictionary.txt");
 		cout << "Dictionary opened - " << boolalpha << in.is_open() << endl;
 		Structure dictionary = Structure();
 		string s;
@@ -42,42 +43,48 @@ int main() {
 			for (auto& j : i)
 				j = s[x++];
 
-
-		vector<future<boost::container::flat_set<string>>> futures;
-		boost::container::flat_set<string> words;
+		boost::container::flat_map<string, vector<pair<int, int>>> k;
+		vector<future<boost::container::flat_map<string, vector<pair<int,int>>>>> futures;
 		for (int i = 0; i < 5; i++)
 			for (int j = 0; j < 5; j++)
 				futures.emplace_back(async(launch::async, [&matrix, i, j, &dictionary]() {
+					vector<pair<int, int>> kk;
+					kk.push_back({ i,j });
+					boost::container::flat_map<string, vector<pair<int, int>>> kkResult;
 					boost::container::flat_set<string> result;
 					string s = "";
 					vector<vector<bool>> ready(5, vector<bool>(5, false));
 					s.push_back(matrix[i][j]);
 					ready[i][j] = true;
-					r(matrix, s, i, j, dictionary, ready, result);
-					return result;
+					r(matrix, s, i, j, dictionary, ready, result, kk, kkResult);
+					return kkResult;
 					}));
 
-		for (auto& f : futures)words.merge(f.get());
-		vector<string> result;
-		for (auto& i : words)result.emplace_back(i);
-		sort(result.begin(), result.end(), [](const string& left, const string& right) {return left.size() > right.size(); });
+		for (auto& f : futures)k.merge(f.get());
+		vector<vector<pair<int,int>>> result;
+		for (auto& i : k)result.emplace_back(i.second);
+		sort(result.begin(), result.end(), [](const vector<pair<int, int>>& left, const vector<pair<int, int>>& right) {return left.size() > right.size(); });
 		cout << result.size() << endl;
-		ofstream out("result.txt");
-		for (auto& i : result)out << i << endl;
+		ofstream out("resultAuto.txt");
+		for (auto& i : result) {
+			for (auto& j : i)
+				out << j.first << j.second;
+			out << endl;
+		}
 		out.close();
 }
 
-bool search(Structure & dictionary, string& s, boost::container::flat_set<string>& words)
+bool search(Structure & dictionary, string& s, boost::container::flat_set<string>& words, vector<pair<int, int>>& kk, boost::container::flat_map<string, vector<pair<int, int>>>& kkResult)
 {
 	Node* srh = dictionary.checkWord(s);
 	if (s.size() == srh->level + 1 && srh->isEndWord)
-		words.emplace(s);
+		if(words.emplace(s).second)kkResult.emplace(s, kk);
 	return srh->nextLevelWords == 0;
 }
 
-void r(const vector<vector<char>>& matrix, string& str, const int& x, const int& y, Structure& dictionary, vector<vector<bool>>& ready, boost::container::flat_set<string>& words)
+void r(const vector<vector<char>>& matrix, string& str, const int& x, const int& y, Structure& dictionary, vector<vector<bool>>& ready, boost::container::flat_set<string>& words, vector<pair<int, int>>& kk, boost::container::flat_map<string, vector<pair<int, int>>>& kkResult)
 {
-	if (search(dictionary, str, words))return;
+	if (search(dictionary, str, words, kk, kkResult))return;
 	if (str.size() == 25)return;
 	vector<pair<int, int>> neighbors;
 	getNeighbors(matrix, x, y, neighbors, ready);
@@ -86,7 +93,9 @@ void r(const vector<vector<char>>& matrix, string& str, const int& x, const int&
 	{
 		str.push_back(matrix[neighbors[i].first][neighbors[i].second]);
 		ready[neighbors[i].first][neighbors[i].second] = true;
-		r(matrix, str, neighbors[i].first, neighbors[i].second, dictionary, ready, words);
+		kk.push_back({ neighbors[i].first, neighbors[i].second });
+		r(matrix, str, neighbors[i].first, neighbors[i].second, dictionary, ready, words, kk, kkResult);
+		kk.pop_back();
 		str.resize(str.size() - 1);
 		ready[neighbors[i].first][neighbors[i].second] = false;
 	}
@@ -152,7 +161,6 @@ void getNeighbors(const vector<vector<char>>& matrix, const int& x, const int& y
 			if (ready[x + 1][3] == false)neighbors.push_back(pair<int, int>{x + 1, 3});
 			if (ready[x - 1][3] == false)neighbors.push_back(pair<int, int>{x - 1, 3});
 		}
-		else cout << "Troubles...neighbors";
 	}
 	else
 	{
