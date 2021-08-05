d53b6e698baf872475ba79df600816174701748b
Nikita <Nikpro200125@gmail.com>
2021-08-05 20:29:58 +0700

    final

---
+++ main.py
@@ -0,0 +1,51 @@
+import mouse
+import pyautogui
+import time
+import subprocess
+from PIL import Image
+import pytesseract
+import cv2
+import keyboard
+
+
+def main():
+    filename = 'screen.png'
+    print('READYREADYREADYREADYREADY')
+    mouse.wait(button=mouse.LEFT, target_types=mouse.DOWN)
+    first_position = mouse.get_position()
+    mouse.wait(button=mouse.LEFT, target_types=mouse.DOWN)
+    second_position = mouse.get_position()
+    step_h = (-first_position[0] + second_position[0]) / 5
+    step_v = (-first_position[1] + second_position[1]) / 5
+    pyautogui.screenshot(filename, region=(first_position[0], first_position[1], second_position[0] - first_position[0],
+                                           second_position[1] - first_position[1]))
+    cv2.imwrite(filename, cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2GRAY))
+    image = Image.open(filename)
+    pre_s = ''
+    for i in range(0, 5):
+        for j in range(0, 5):
+            im = image.crop((step_h * (j + 0.2), step_v * (i + 0.15), step_h * (j + 1 - 0.2), step_v * (i + 1 - 0.15)))
+            im.save(filename)
+            pre_s += (pytesseract.image_to_string(im, lang='rus', config='--psm 10')[0])
+    with open('input.txt', 'w') as out:
+        out.write(pre_s.lower())
+    print(pre_s)
+    subprocess.Popen('szs.exe').wait()
+    start_time = time.time()
+    time.sleep(0.6)
+    with open("resultAuto.txt", 'r') as inp:
+        for x in inp.readlines():
+            if time.time() - start_time > 115 or keyboard.is_pressed('space'): break
+            mouse.release(button=mouse.LEFT)
+            mouse.move(first_position[0] + (int(x[1]) + 0.5) * step_h,
+                       first_position[1] + (int(x[0]) + 0.5) * step_v, duration=0.04)
+            mouse.press(button=mouse.LEFT)
+            for i in range(2, len(x) - 1, 2):
+                mouse.move((int(x[i + 1]) - int(x[i - 1])) * step_h, (int(x[i]) - int(x[i - 2])) * step_v,
+                           absolute=False, duration=0.04)
+            mouse.release(button=mouse.LEFT)
+            time.sleep(0.25)
+
+
+if __name__ == '__main__':
+    main()
